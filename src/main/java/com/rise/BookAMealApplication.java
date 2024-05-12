1db9c16738fc7fcae175ca5cce2a3bf51f83356f
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2024-05-12 12:59:38 +0530

    Initial Files for Book My meal Backend

---
+++ src/main/java/com/rise/BookAMealApplication.java
@@ -0,0 +1,13 @@
+package com.rise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rise/BookAMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
