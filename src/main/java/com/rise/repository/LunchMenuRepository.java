9c308fa4a7c1225de4dcbf7d3c8ba55d2bf92b90
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2024-06-03 00:03:53 +0530

    Removed Lunch menu Api

--- src/main/java/com/rise/repository/LunchMenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rise.repository;
-
-import com.rise.entity.LunchMenu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LunchMenuRepository extends JpaRepository<LunchMenu,Long> {
-}
